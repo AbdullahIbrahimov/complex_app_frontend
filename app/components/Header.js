c0c39093714315a9d265845a6ad076c13cfa9aba
YadullaIbrahimov <YadullaIbrahimov@users.noreply.github.com>
2021-07-11 12:07:24 -0400

    preparefrontend for deployment

---
+++ app/components/Header.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import HeaderLoggedOut from "./HeaderLoggedOut";
+import HeaderLoggedIn from "./HeaderLoggedIn";
+import stateContext from "../stateContext";
+function Header(props) {
+  const appState = useContext(stateContext);
+  const headerContent = appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />;
+  return (
+    <header className="header-bar bg-primary mb-3">
+      <div className="container d-flex flex-column flex-md-row align-items-center p-3">
+        <h4 className="my-0 mr-md-auto font-weight-normal">
+          <a href="/" className="text-white">
+            {" "}
+            ComplexApp!{" "}
+          </a>
+        </h4>
+        {!props.staticEmpty ? headerContent : ""}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
